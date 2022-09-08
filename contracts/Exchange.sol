be0c6971a4950ed7a36d45c736d4c34259475003
Neeraj <neerajk@somnoware.com>
2022-09-08 11:59:11 +0530

    📦 NEW: add cancelOrder and tests

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -9,6 +9,7 @@ contract Exchange {
     uint256 public orderCount;
     mapping(address => mapping(address => uint256)) public tokens;
     mapping(uint256 => _Order) public orders;
+    mapping(uint256 => bool) public orderCancelled;
 
     struct _Order{
         uint256 id;
@@ -23,6 +24,15 @@ contract Exchange {
     event Deposit(address token, address user, uint256 amount, uint256 balance);
     event Withdraw(address token, address user, uint256 amount, uint256 balance);
     event Order(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
+    );
+    event Cancel(
         uint256 id,
         address user,
         address tokenGet,
@@ -74,5 +84,19 @@ contract Exchange {
                    _amountGive,
                    block.timestamp
                 );
+    }
+    function cancelOrder(uint256 _id) public {
+        _Order storage _order = orders[_id];
+        require(address(_order.user) == msg.sender);
+        require(_order.id == _id);
+        orderCancelled[_id] = true;
+        emit Cancel(_order.id,
+                   msg.sender,
+                   _order.tokenGet,
+                   _order.amountGet,
+                   _order.tokenGive,
+                   _order.amountGive,
+                   block.timestamp
+                );    
     }
 }
