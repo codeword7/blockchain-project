a53309ce34dc996e08dcc10fe83e23ff127b29d2
Neeraj <neerajk@somnoware.com>
2022-09-08 17:13:14 +0530

    📦 NEW: add deploy, seed scripts and config.json

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,9 +1,27 @@
 async function main() {
-  const Token = await ethers.getContractFactory("Token");
+  console.log(`Preparing deployment...\n`);
 
-  const token = await Token.deploy();
-  await token.deployed();
-  console.log(`Token deployed to: ${token.address}`)
+  const Token = await ethers.getContractFactory("Token");
+  const Exchange = await ethers.getContractFactory("Exchange");
+
+  const accounts = await ethers.getSigners();
+  console.log(`Accounts fetched:\n${accounts[0].address}\n${accounts[1].address}\n`);
+
+  const nrj = await Token.deploy('Neeraj', 'NRJ', "1000000");
+  await nrj.deployed();
+  console.log(`NRJ deployed to: ${nrj.address}`)
+
+  const mETH = await Token.deploy('mETH', 'mETH', "1000000");
+  await mETH.deployed();
+  console.log(`mETH deployed to: ${mETH.address}`)
+
+  const mDAI = await Token.deploy('mDAI', 'mDAI', "1000000");
+  await mDAI.deployed();
+  console.log(`mDAI deployed to: ${mDAI.address}`)
+
+  const exchange = await Exchange.deploy(accounts[1].address, 10);
+  await exchange.deployed();
+  console.log(`Exchange deployed to: ${exchange.address}`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
