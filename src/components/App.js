a2d379571be0b7709de64d9fdc5c5bbac7aa457c
Neeraj <neerajk@somnoware.com>
2022-09-22 12:56:11 +0530

    📦 NEW: add price chart

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import Markets from './Markets';
 import Balance from './Balance';
 import Order from './Order';
 import OrderBook from './OrderBook';
+import PriceChart from './PriceChart';
 
 function App() {
 
@@ -55,7 +56,7 @@ function App() {
         </section>
         <section className='exchange__section--right grid'>
 
-          {/* PriceChart */}
+          <PriceChart />
 
           {/* Transactions */}
 
